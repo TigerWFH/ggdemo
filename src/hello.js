76814ade964caac9d363de2a89a102298887d236
wangfanghua <wangfanghua@jk.cn>
2018-01-25 10:55:16 +0800

    调通超链接，需要给a标签添加target属性

--- src/hello.js
+++ src/hello.js
@@ -1,8 +1,12 @@
 window.addEventListener("DOMContentLoaded", function () {
     // 区分DOMContentLoaded----load
     let target = window.document.getElementById('baidu');
+    let taobao = window.document.getElementById('taobao');
 
     target.addEventListener('click', function () {
         window.open("https://www.baidu.com");
+    });
+    taobao.addEventListener('clcik', function(){
+        window.open('./tt.html');
     })
 })
